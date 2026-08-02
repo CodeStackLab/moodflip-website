8a2ff96a8f8f999098e2f618f051fc660e475c12
DevHexo-coder <khanhostinger@gmail.com>
2026-08-03 04:33:09 +0530

    Equalize heights of all 3 dashboard columns: stretch layout for moodPanel, flipCard, and morePanel

--- app/page.tsx
+++ app/page.tsx
@@ -198,12 +198,14 @@ export default function HomePage() {
             <div className={styles.cardToolbar}>
               <span />
               <div>
-                <IconButton label="Save"><span>♡</span></IconButton>
-                <IconButton label="Share"><span>↗</span></IconButton>
+                <button type="button" className={styles.topIconBtn}><span>🔖</span> Save</button>
+                <button type="button" className={styles.topIconBtn}><span>🔗</span> Share</button>
               </div>
             </div>
 
-            <div className={styles.flipIntro}>✓&nbsp;&nbsp; Here&apos;s your <strong>positive flip</strong></div>
+            <div className={styles.flipIntro}>
+              <span className={styles.checkCircle}>✓</span> Here&apos;s your <strong>positive flip</strong>
+            </div>
 
             <div className={styles.sunBadge} aria-hidden="true">
               <span className={styles.sunCore}>🌅</span>
@@ -220,41 +222,57 @@ export default function HomePage() {
               <div className={styles.hillBack} />
               <div className={styles.hillFront} />
               
-              {/* Left Plant Branch SVG */}
+              {/* Left Plant Branch SVG - Taller & Fuller */}
               <div className={styles.plantLeft}>
-                <svg width="100" height="240" viewBox="0 0 100 240" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <svg width="120" height="320" viewBox="0 0 120 320" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <defs>
                     <linearGradient id="leafGradLeft" x1="0%" y1="100%" x2="100%" y2="0%">
-                      <stop offset="0%" stopColor="#f38ba0" stopOpacity="0.85" />
-                      <stop offset="55%" stopColor="#d962a7" stopOpacity="0.8" />
-                      <stop offset="100%" stopColor="#8d49c7" stopOpacity="0.75" />
+                      <stop offset="0%" stopColor="#f77f88" stopOpacity="0.9" />
+                      <stop offset="45%" stopColor="#e0589a" stopOpacity="0.85" />
+                      <stop offset="85%" stopColor="#9b48c4" stopOpacity="0.8" />
+                      <stop offset="100%" stopColor="#673eb8" stopOpacity="0.75" />
                     </linearGradient>
                   </defs>
-                  <path d="M 25 240 Q 35 150 70 25" stroke="url(#leafGradLeft)" strokeWidth="4" strokeLinecap="round" />
-                  <path d="M 70 25 C 55 10, 30 15, 38 42 C 46 69, 64 42, 70 25 Z" fill="url(#leafGradLeft)" />
-                  <path d="M 52 75 C 24 65, 8 88, 24 108 C 40 128, 56 95, 52 75 Z" fill="url(#leafGradLeft)" />
-                  <path d="M 60 98 C 86 82, 100 105, 84 125 C 68 145, 52 115, 60 98 Z" fill="url(#leafGradLeft)" />
-                  <path d="M 38 140 C 12 130, -4 152, 12 172 C 28 192, 44 160, 38 140 Z" fill="url(#leafGradLeft)" />
-                  <path d="M 44 162 C 68 146, 84 168, 68 188 C 52 208, 36 178, 44 162 Z" fill="url(#leafGradLeft)" />
+                  {/* Main curved stem */}
+                  <path d="M 30 320 Q 45 180 85 30" stroke="url(#leafGradLeft)" strokeWidth="4.5" strokeLinecap="round" />
+                  {/* Top terminal leaf */}
+                  <path d="M 85 30 C 65 10, 35 18, 48 50 C 61 82, 80 50, 85 30 Z" fill="url(#leafGradLeft)" />
+                  {/* Pair 1 */}
+                  <path d="M 68 85 C 32 72, 12 98, 32 122 C 52 146, 72 108, 68 85 Z" fill="url(#leafGradLeft)" />
+                  <path d="M 76 112 C 106 95, 122 122, 102 144 C 82 166, 64 132, 76 112 Z" fill="url(#leafGradLeft)" />
+                  {/* Pair 2 */}
+                  <path d="M 52 160 C 18 148, -2 175, 18 198 C 38 221, 58 185, 52 160 Z" fill="url(#leafGradLeft)" />
+                  <path d="M 58 185 C 90 168, 108 195, 88 218 C 68 241, 48 206, 58 185 Z" fill="url(#leafGradLeft)" />
+                  {/* Pair 3 */}
+                  <path d="M 40 235 C 8 222, -10 250, 10 272 C 30 294, 50 258, 40 235 Z" fill="url(#leafGradLeft)" />
+                  <path d="M 45 258 C 76 242, 94 268, 74 290 C 54 312, 34 278, 45 258 Z" fill="url(#leafGradLeft)" />
                 </svg>
               </div>
 
-              {/* Right Plant Branch SVG */}
+              {/* Right Plant Branch SVG - Taller & Fuller */}
               <div className={styles.plantRight}>
-                <svg width="100" height="240" viewBox="0 0 100 240" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <svg width="120" height="320" viewBox="0 0 120 320" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <defs>
                     <linearGradient id="leafGradRight" x1="0%" y1="100%" x2="100%" y2="0%">
-                      <stop offset="0%" stopColor="#f38ba0" stopOpacity="0.85" />
-                      <stop offset="55%" stopColor="#d962a7" stopOpacity="0.8" />
-                      <stop offset="100%" stopColor="#8d49c7" stopOpacity="0.75" />
+                      <stop offset="0%" stopColor="#f77f88" stopOpacity="0.9" />
+                      <stop offset="45%" stopColor="#e0589a" stopOpacity="0.85" />
+                      <stop offset="85%" stopColor="#9b48c4" stopOpacity="0.8" />
+                      <stop offset="100%" stopColor="#673eb8" stopOpacity="0.75" />
                     </linearGradient>
                   </defs>
-                  <path d="M 75 240 Q 65 150 30 25" stroke="url(#leafGradRight)" strokeWidth="4" strokeLinecap="round" />
-                  <path d="M 30 25 C 45 10, 70 15, 62 42 C 54 69, 36 42, 30 25 Z" fill="url(#leafGradRight)" />
-                  <path d="M 48 75 C 76 65, 92 88, 76 108 C 60 128, 44 95, 48 75 Z" fill="url(#leafGradRight)" />
-                  <path d="M 40 98 C 14 82, 0 105, 16 125 C 32 145, 48 115, 40 98 Z" fill="url(#leafGradRight)" />
-                  <path d="M 62 140 C 88 130, 104 152, 88 172 C 72 192, 56 160, 62 140 Z" fill="url(#leafGradRight)" />
-                  <path d="M 56 162 C 32 146, 16 168, 32 188 C 48 208, 64 178, 56 162 Z" fill="url(#leafGradRight)" />
+                  {/* Main curved stem */}
+                  <path d="M 90 320 Q 75 180 35 30" stroke="url(#leafGradRight)" strokeWidth="4.5" strokeLinecap="round" />
+                  {/* Top terminal leaf */}
+                  <path d="M 35 30 C 55 10, 85 18, 72 50 C 59 82, 40 50, 35 30 Z" fill="url(#leafGradRight)" />
+                  {/* Pair 1 */}
+                  <path d="M 52 85 C 88 72, 108 98, 88 122 C 68 146, 48 108, 52 85 Z" fill="url(#leafGradRight)" />
+                  <path d="M 44 112 C 14 95, -2 122, 18 144 C 38 166, 56 132, 44 112 Z" fill="url(#leafGradRight)" />
+                  {/* Pair 2 */}
+                  <path d="M 68 160 C 102 148, 122 175, 102 198 C 82 221, 62 185, 68 160 Z" fill="url(#leafGradRight)" />
+                  <path d="M 62 185 C 30 168, 12 195, 32 218 C 52 241, 72 206, 62 185 Z" fill="url(#leafGradRight)" />
+                  {/* Pair 3 */}
+                  <path d="M 80 235 C 112 222, 130 250, 110 272 C 90 294, 70 258, 80 235 Z" fill="url(#leafGradRight)" />
+                  <path d="M 75 258 C 44 242, 26 268, 46 290 C 66 312, 86 278, 75 258 Z" fill="url(#leafGradRight)" />
                 </svg>
               </div>
             </div>
@@ -289,7 +307,7 @@ export default function HomePage() {
 
               <div className={styles.whyBlock}>
                 <div>♡&nbsp;&nbsp; Why this helps</div>
-                <p>✦&nbsp;&nbsp; Deep breathing activates your body&apos;s natural calm response<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;and helps reset your mind.</p>
+                <p>+&nbsp;&nbsp; Deep breathing activates your body&apos;s natural calm response<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;and helps reset your mind.</p>
               </div>
             </div>
 
